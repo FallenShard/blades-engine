3ffc518b4de421ad8bb44f03c4ca1cd20b4b8f80
FallenShard <nemanjabartolovic@yahoo.com>
2014-10-26 21:20:12 +0100

    Change stuff to accomodate previous additions

--- OpenGLSandbox/OpenGLSandbox/src/Scenes/RobotArmScene.cpp
+++ OpenGLSandbox/OpenGLSandbox/src/Scenes/RobotArmScene.cpp
@@ -3,6 +3,7 @@
 #include "Models/SceneNode.h"
 #include "Models/Prism.h"
 #include "Models/RoboticArm.h"
+#include "Models/Sphere.h"
 
 #include "Renderer/ShaderManager.h"
 
@@ -12,8 +13,9 @@ namespace
 {
     GLenum error = false;
 
-    SceneNode* g_root = nullptr;
+    RoboticArm* g_root = nullptr;
     Prism* g_prism = nullptr;
+    Sphere* g_sphere = nullptr;
 
     ShaderManager* manager = nullptr;
     ShaderProgram* prog = nullptr;
@@ -35,7 +37,9 @@ RobotArmScene::RobotArmScene(Window* window)
 
 RobotArmScene::~RobotArmScene()
 {
+    delete g_prism;
     delete g_root;
+    delete g_sphere;
     delete manager;
 }
 
@@ -52,6 +56,7 @@ void RobotArmScene::prepare()
     prog->getUniformAttribute("cameraToClipMatrix");
 
     m_cameraController.setShaderProgram(prog);
+    m_cameraController.setPosition(glm::vec3(0.f, 32.f, 36.f));
 
     m_planeGrid.setProgram(prog);
 
@@ -100,6 +105,12 @@ void RobotArmScene::prepare()
     m_robotArm.attachShaderProgram(prog);
     g_root = new RoboticArm(prog);
     g_prism = new Prism(cArray, prog);
+    g_prism->scale(glm::vec3(1.5f, 1.5f, 1.5f));
+
+    g_sphere = new Sphere(2.f, 20, 20, prog);
+    
+    g_sphere->translate(glm::vec3(10.f, 0.f, -10.f));
+    //g_prism->translate(glm::vec3(0.f, 0.f, -5.f));
 }
 
 namespace
@@ -122,43 +133,54 @@ void RobotArmScene::handleEvents(const Event& event)
         {
         case Keyboard::NumPad4:
         {
-            yaw += 10.f;
-            g_root->rotateY(inc);
+            //yaw += 10.f;
+            //g_root->rotateY(inc);
             //g_root->applyTransformation();
+            //inc += 10.f;
+            //g_prism->translate(glm::vec3(-5.f, 0.f, 0.f));
+            g_sphere->setPrimitiveType(GL_TRIANGLES);
+            
             break;
         }
         case Keyboard::NumPad6:
         {
-            yaw -= 10.f;
-            g_root->rotateY(-inc);
+            //g_prism->translate(glm::vec3(5.f, 0.f, 0.f));
+            //yaw -= 10.f;
+            //g_root->rotateY(-inc);
             //g_root->applyTransformation();
+            g_sphere->setPrimitiveType(GL_LINE_LOOP);
             break;
         }
         case Keyboard::NumPad5:
         {
-            pitch += 10.f;
-            g_root->rotateX(-inc);
+            //pitch += 10.f;
+            //g_root->rotateX(-inc);
             //g_root->applyTransformation();
+            //g_prism->translate(glm::vec3(0.f, 0.f, 5.f));
+            g_sphere->setPrimitiveType(GL_POINTS);
             break;
         }
         case Keyboard::NumPad8:
         {
-            pitch -= 10.f;
-            g_root->rotateX(inc);
+            //g_prism->translate(glm::vec3(0.f, 0.f, -5.f));
+            //pitch -= 10.f;
+            //g_root->rotateX(inc);
             //g_root->applyTransformation();
             break;
         }
         case Keyboard::NumPad7:
         {
-            roll += 10.f;
-            g_root->rotateZ(inc);
+            //g_prism->rotateY(9.f);
+            //roll += 10.f;
+            //g_root->rotateZ(inc);
             //g_root->applyTransformation();
             break;
         }
         case Keyboard::NumPad9:
         {
-            roll -= 10.f;
-            g_root->rotateZ(-inc);
+            //g_prism->rotateY(-9.f);
+            //roll -= 10.f;
+            //g_root->rotateZ(-inc);
             //g_root->applyTransformation();
             break;
         }
@@ -167,23 +189,83 @@ void RobotArmScene::handleEvents(const Event& event)
     };
 }
 
+namespace
+{
+    float cubeTransX = 0.f;
+    float cubeTranxY = 0.f;
+    float cubeSpeed = 10.f;
+}
+
 void RobotArmScene::update(float timeDelta)
 {
     m_timePassed += timeDelta;
 
     m_cameraController.update(timeDelta);
 
+    g_root->update(timeDelta);
+
     if (Keyboard::isKeyPressed(Keyboard::A))
     {
         m_robotArm.moveBase(true);
-        g_prism->rotateY(2.f);
+        //g_prism->rotateY(2.f);
         
+    }
+
+    if (Keyboard::isKeyPressed(Keyboard::I))
+    {
+        g_sphere->translate(glm::vec3(-5.f * timeDelta, 0.f, 0.f));
+    }
+
+    if (Keyboard::isKeyPressed(Keyboard::O))
+    {
+        g_sphere->translate(glm::vec3(5.f * timeDelta, 0.f, 0.f));
+    }
+
+    if (Keyboard::isKeyPressed(Keyboard::NumPad7))
+    {
+        //g_prism->rotateY(1.5f);
+    }
+
+    if (Keyboard::isKeyPressed(Keyboard::NumPad9))
+    {
+        //g_prism->rotateY(-1.5f);
+    }
+
+    if (Keyboard::isKeyPressed(Keyboard::NumPad4))
+    {
+        cubeTransX -= cubeSpeed * timeDelta;
+        //g_prism->translate(glm::vec3(cubeTransX, 0.f, 0.f));
+        g_prism->rotateY(3.f);
+    }
+
+    if (Keyboard::isKeyPressed(Keyboard::NumPad6))
+    {
+        cubeTransX += cubeSpeed * timeDelta;
+        //g_prism->translate(glm::vec3(cubeTransX, 0.f, 0.f));
+        g_prism->rotateY(-3.f);
+    }
+
+    if (Keyboard::isKeyPressed(Keyboard::NumPad5))
+    {
+        //g_prism->translate(glm::vec3(0.f, 0.f, 0.16f));
+        g_prism->rotateX(-3.f);
+    }
+
+    if (Keyboard::isKeyPressed(Keyboard::NumPad2))
+    {
+        //g_prism->translate(glm::vec3(0.f, 0.f, 0.16f));
+        g_prism->rotateX(3.f);
+    }
+
+    if (Keyboard::isKeyPressed(Keyboard::NumPad8))
+    {
+        g_prism->translate(glm::vec3(0.f, 0.f, -0.16f));
     }
         
     if (Keyboard::isKeyPressed(Keyboard::D))
     {
         m_robotArm.moveBase(false);
-        g_prism->rotateY(-2.f);
+        //g_prism->rotateY(-2.f);
     }
         
     if (Keyboard::isKeyPressed(Keyboard::W))
@@ -245,12 +327,14 @@ void RobotArmScene::render()
     prog->setUniformAttribute("modelToWorldMatrix", 1, GL_FALSE, glm::value_ptr(glm::mat4(1.f)));
     
     m_planeGrid.render();
+    
 
-    m_vertexArrays["Cube"]->bind();
-    m_vertexArrays["Cube"]->renderIndexed();
+    //m_vertexArrays["Cube"]->bind();
+    //m_vertexArrays["Cube"]->renderIndexed();
 
     g_root->render();
     g_prism->render();
+    g_sphere->render();
     m_robotArm.render();
 }
 
